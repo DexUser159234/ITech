b56422e07741eb5d8dbca26565bda51e00f6b5e6
DexUser159234 <92317423+DexUser159234@users.noreply.github.com>
2022-03-16 18:34:17 +0300

    Update Trojain.bat

--- Trojain.bat
+++ Trojain.bat
@@ -1,1 +1,1 @@
-writefile("HelloXD" , "@echo off & echo HelloXD & echo Press any key to close... & pause>nul & exit /B")
+writefile("HelloXD","@echo off & echo HelloXD & echo Press any key to close... & pause>nul & exit /B")
